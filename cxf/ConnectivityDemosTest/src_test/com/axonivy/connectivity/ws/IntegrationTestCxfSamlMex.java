ed0f9ef19fdd38a13c4721ae273379fd4ee7f3fa
Reguel <reguel.wermelinger@ivyteam.ch>
2018-07-30 16:29:08 +0200

    fix imports

--- cxf/ConnectivityDemosTest/src_test/com/axonivy/connectivity/ws/IntegrationTestCxfSamlMex.java
+++ cxf/ConnectivityDemosTest/src_test/com/axonivy/connectivity/ws/IntegrationTestCxfSamlMex.java
@@ -2,15 +2,8 @@ package com.axonivy.connectivity.ws;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.security.auth.callback.Callback;
-import javax.security.auth.callback.CallbackHandler;
-import javax.security.auth.callback.UnsupportedCallbackException;
 
 import org.apache.cxf.endpoint.Client;
 import org.apache.cxf.endpoint.EndpointImpl;
@@ -19,10 +12,6 @@ import org.apache.cxf.interceptor.Interceptor;
 import org.apache.cxf.ws.policy.AssertionBuilderRegistry;
 import org.apache.cxf.ws.policy.PolicyInterceptorProviderRegistry;
 import org.apache.cxf.ws.security.wss4j.PolicyBasedWSS4JOutInterceptor;
-import org.apache.cxf.ws.security.wss4j.WSS4JOutInterceptor;
-import org.apache.wss4j.common.ext.WSPasswordCallback;
-import org.apache.wss4j.dom.WSConstants;
-import org.apache.wss4j.dom.handler.WSHandlerConstants;
 import org.junit.Test;
 
 import com.axonivy.connectivity.ws.saml.SAPEnabledAssertionBuilder;
